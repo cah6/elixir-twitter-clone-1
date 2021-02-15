1a18f816cb849c2c08f5b7b072ba65d9bcc83787
Christian Henry <christian.henry92@gmail.com>
2021-02-15 17:00:12 -0500

    copy in generated `posts` routes

--- lib/chirp_web/router.ex
+++ lib/chirp_web/router.ex
@@ -2,22 +2,29 @@ defmodule ChirpWeb.Router do
   use ChirpWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_live_flash
-    plug :put_root_layout, {ChirpWeb.LayoutView, :root}
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_live_flash)
+    plug(:put_root_layout, {ChirpWeb.LayoutView, :root})
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", ChirpWeb do
-    pipe_through :browser
+    pipe_through(:browser)
 
-    live "/", PageLive, :index
+    live("/", PageLive, :index)
+
+    live("/posts", PostLive.Index, :index)
+    live("/posts/new", PostLive.Index, :new)
+    live("/posts/:id/edit", PostLive.Index, :edit)
+
+    live("/posts/:id", PostLive.Show, :show)
+    live("/posts/:id/show/edit", PostLive.Show, :edit)
   end
 
   # Other scopes may use custom stacks.
@@ -36,8 +43,8 @@ defmodule ChirpWeb.Router do
     import Phoenix.LiveDashboard.Router
 
     scope "/" do
-      pipe_through :browser
-      live_dashboard "/dashboard", metrics: ChirpWeb.Telemetry
+      pipe_through(:browser)
+      live_dashboard("/dashboard", metrics: ChirpWeb.Telemetry)
     end
   end
 end
